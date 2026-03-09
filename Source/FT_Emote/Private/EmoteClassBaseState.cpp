4b3252375e25cf75da9a9d3480c44a2fcb30540a
HDavies2605 <hdavies.2605@gmail.com>
2026-03-09 14:34:09 +0000

    EmoteClassBaseState

---
+++ Source/FT_Emote/Private/EmoteClassBaseState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EmoteClassBaseState.h"
+
+void UEmoteClassBaseState::SetController(EmoteClass* MainController)
+{
+	Controller = MainController;
+}
