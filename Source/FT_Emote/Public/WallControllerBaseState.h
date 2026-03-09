38c9e5fd8b76df1f2c9e3db8fcecaec6e7e6b2bc
connahKendrickMMU <44666644+connahKendrickMMU@users.noreply.github.com>
2026-03-09 10:01:37 +0000

    delete old comment

--- Source/FT_Emote/Public/WallControllerBaseState.h
+++ Source/FT_Emote/Public/WallControllerBaseState.h
@@ -10,7 +10,6 @@
  * Common errors for this is:
  * 1. Not marking the class as abstract, and not blueprintable
  * 2. Not implementing the pure virtual functions or trying to set the = 0 in the cpp
- * 3. Not providing a protected constructor to prevent instantiation of the base class
  * 3. Trying to instantiate the abstract class directly, which will result in a compile-time error.
  * 4. Not including the necessary header files for the derived classes that will implement this base state.
  */
@@ -24,8 +23,6 @@ protected:
 	AWallController* Controller;
 public:
 	void SetController(AWallController* ControllerMain);
-	// note to set to abstract i set the methods to 0, otherwise know as a pure virtual function, 
-	// which means that the derived class must implement these methods, otherwise it will also be abstract and cannot be instantiated.
 	virtual void EnterState();
 	virtual void ExitState();
 	virtual void UpdateState(float DeltaTime);
