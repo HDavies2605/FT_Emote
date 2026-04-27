f89e44624decfd1b4824c2d989006342c89c118b
HDavies2605 <hdavies.2605@gmail.com>
2026-04-27 13:09:26 +0100

    Revert "EmoteClassBaseState"
    This reverts commit 4b3252375e25cf75da9a9d3480c44a2fcb30540a.

--- Source/FT_Emote/Private/EmoteClassBaseState.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "EmoteClassBaseState.h"
-
-void UEmoteClassBaseState::SetController(EmoteClass* MainController)
-{
-	Controller = MainController;
-}
